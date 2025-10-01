e2ffe8f1fdcb9f122e934e3fb96f7d9628961c34
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-01 14:22:57 +0200

    Добавьте файлы проекта.

---
+++ EmployeePortal/Controllers/EmployeesController.cs
@@ -0,0 +1,49 @@
+﻿using EmployeePortal.Data;
+using EmployeePortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+
+        //GET: api/employees
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployee()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return employees;
+        }
+
+        //GET: api/employees/5
+        //[HttpGet]
+        //public async Task<ActionResult<List<Employee>>> GetEmployes(Guid id)
+        //{
+        //    var employees = await _context.Employees.ToListAsync();
+        //    return employees;
+        //}
+
+        //Post
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if(employee.Id == Guid.Empty)
+            {
+                employee.Id = Guid.NewGuid();
+            }
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+    }
+}
---
+++ EmployeePortal/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeePortal.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; } //обязательно к заполнению
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
